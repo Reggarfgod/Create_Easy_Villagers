3cb40bc44df393d9e370c5ad54890fc4a4b29c67
AnoopSingh <Reggerfgod@gmail.com>
2026-08-23 18:34:30 +0530

    fix: shift converter villager and zombie entities forward away from the back wall and adjust farm back plate bounds

---
+++ src/main/java/com/reggarf/mods/create_easy_villagers/mixin/ConverterRendererMixin.java
@@ -0,0 +1,29 @@
+package com.reggarf.mods.create_easy_villagers.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import de.maxhenkel.easyvillagers.blocks.tileentity.ConverterTileentity;
+import de.maxhenkel.easyvillagers.blocks.tileentity.render.ConverterRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ConverterRenderer.class)
+public abstract class ConverterRendererMixin {
+
+    @Redirect(
+            method = "render(Lde/maxhenkel/easyvillagers/blocks/tileentity/ConverterTileentity;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V",
+            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V", ordinal = 1)
+    )
+    private void create_easy_villagers$offsetVillager(PoseStack ms, double x, double y, double z) {
+        ms.translate(x, y, 0.12D);
+    }
+
+    @Redirect(
+            method = "render(Lde/maxhenkel/easyvillagers/blocks/tileentity/ConverterTileentity;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V",
+            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;translate(DDD)V", ordinal = 3)
+    )
+    private void create_easy_villagers$offsetZombie(PoseStack ms, double x, double y, double z) {
+        ms.translate(x, y, 0.12D);
+    }
+}
