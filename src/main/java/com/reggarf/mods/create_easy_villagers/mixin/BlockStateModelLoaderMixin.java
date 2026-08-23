1833373d01664ac1295a9aea6940c5c9d6068294
AnoopSingh <Reggerfgod@gmail.com>
2026-08-23 18:27:13 +0530

    fix: redirect blockstates and models map at constructor initialization

--- src/main/java/com/reggarf/mods/create_easy_villagers/mixin/BlockStateModelLoaderMixin.java
+++ src/main/java/com/reggarf/mods/create_easy_villagers/mixin/BlockStateModelLoaderMixin.java
@@ -2,36 +2,34 @@ package com.reggarf.mods.create_easy_villagers.mixin;
 
 import net.minecraft.client.resources.model.BlockStateModelLoader;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.StateDefinition;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Mixin(BlockStateModelLoader.class)
 public abstract class BlockStateModelLoaderMixin {
 
-    @Shadow
-    @Final
-    private Map<ResourceLocation, List<BlockStateModelLoader.LoadedJson>> blockStateResources;
-
     @ModifyVariable(
-            method = "loadBlockStateDefinition",
+            method = "<init>",
             at = @At("HEAD"),
-            argsOnly = true
+            argsOnly = true,
+            ordinal = 0
     )
-    private ResourceLocation redirectEasyVillagersBlockstate(ResourceLocation id) {
-        if ("easy_villagers".equals(id.getNamespace())) {
-            ResourceLocation customLoc = ResourceLocation.fromNamespaceAndPath("create_easy_villagers", id.getPath());
-            if (this.blockStateResources.containsKey(customLoc)) {
-                return customLoc;
+    private static Map<ResourceLocation, List<BlockStateModelLoader.LoadedJson>> create_easy_villagers$redirectBlockstateMap(
+            Map<ResourceLocation, List<BlockStateModelLoader.LoadedJson>> map
+    ) {
+        Map<ResourceLocation, List<BlockStateModelLoader.LoadedJson>> newMap = new HashMap<>(map);
+        for (Map.Entry<ResourceLocation, List<BlockStateModelLoader.LoadedJson>> entry : map.entrySet()) {
+            ResourceLocation key = entry.getKey();
+            if ("create_easy_villagers".equals(key.getNamespace())) {
+                ResourceLocation easyVillagersKey = ResourceLocation.fromNamespaceAndPath("easy_villagers", key.getPath());
+                newMap.put(easyVillagersKey, entry.getValue());
             }
         }
-        return id;
+        return newMap;
     }
 }
--- src/main/java/com/reggarf/mods/create_easy_villagers/mixin/ModelBakeryMixin.java
+++ src/main/java/com/reggarf/mods/create_easy_villagers/mixin/ModelBakeryMixin.java
@@ -3,33 +3,33 @@ package com.reggarf.mods.create_easy_villagers.mixin;
 import net.minecraft.client.renderer.block.model.BlockModel;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Mixin(ModelBakery.class)
 public abstract class ModelBakeryMixin {
 
-    @Shadow
-    @Final
-    private Map<ResourceLocation, BlockModel> modelResources;
-
     @ModifyVariable(
-            method = "loadBlockModel",
+            method = "<init>",
             at = @At("HEAD"),
-            argsOnly = true
+            argsOnly = true,
+            ordinal = 0
     )
-    private ResourceLocation redirectEasyVillagersModel(ResourceLocation location) {
-        if ("easy_villagers".equals(location.getNamespace())) {
-            ResourceLocation customLoc = ResourceLocation.fromNamespaceAndPath("create_easy_villagers", location.getPath());
-            if (this.modelResources.containsKey(customLoc)) {
-                return customLoc;
+    private static Map<ResourceLocation, BlockModel> create_easy_villagers$redirectModelMap(
+            Map<ResourceLocation, BlockModel> map
+    ) {
+        Map<ResourceLocation, BlockModel> newMap = new HashMap<>(map);
+        for (Map.Entry<ResourceLocation, BlockModel> entry : map.entrySet()) {
+            ResourceLocation key = entry.getKey();
+            if ("create_easy_villagers".equals(key.getNamespace())) {
+                ResourceLocation easyVillagersKey = ResourceLocation.fromNamespaceAndPath("easy_villagers", key.getPath());
+                newMap.put(easyVillagersKey, entry.getValue());
             }
         }
-        return location;
+        return newMap;
     }
 }
